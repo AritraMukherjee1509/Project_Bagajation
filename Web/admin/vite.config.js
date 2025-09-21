cef8119202ba580ee3f97b8baa4c1a4f054cf9c3
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-09-22 05:12:01 +0530

    feat: Add preview configuration to vite.config.js for local development

--- Web/admin/vite.config.js
+++ Web/admin/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 5174, // Render overrides with $PORT
+    allowedHosts: [
+      'project-bagajation-admin.onrender.com'
+    ]
+  }
 })
